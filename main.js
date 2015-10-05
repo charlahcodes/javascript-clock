346143333bc0a9ec5711aaee94c3fa7f92d0ddeb
Charlie Miller <charlie@thecharlah.com>
2015-10-05 14:15:44 -0400

    clock style

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-
+(function () {
 
 var intervalID = window.setInterval(function displayClock() {
   var newDate = new Date();
   var hours = newDate.getHours();
   var minutes = newDate.getMinutes();
   var seconds = newDate.getSeconds();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  };
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  };
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  };
+
   var time = hours + ":" + minutes + ":" + seconds;
   $('#clock').text(time);
 
 }, 1000);
 
+
+})();
